b97cb07a0dbbfb8c1c6ee40dea67998f5d44be3f
João M. <jvmarchette02003@gmail.com>
2025-07-03 23:02:51 -0300

    correções mapping e adição app properties

--- src/main/java/com/syncteam/buscaEmpregoPAOO/mappers/CandidateMapper.java
+++ src/main/java/com/syncteam/buscaEmpregoPAOO/mappers/CandidateMapper.java
@@ -1,18 +1,20 @@
 package com.syncteam.buscaEmpregoPAOO.mappers;
 
-import com.syncteam.buscaEmpregoPAOO.dtos.CandidateDto;
-import com.syncteam.buscaEmpregoPAOO.entities.Candidate;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
+import com.syncteam.buscaEmpregoPAOO.dtos.CandidateDto;
+import com.syncteam.buscaEmpregoPAOO.entities.Candidate;
+
 @Mapper(componentModel = "spring")
 public interface CandidateMapper {
+    @Mapping(target = "registrationDate", ignore = true)
     Candidate toEntity(CandidateDto dto);
 
     CandidateDto toDTO(Candidate candidate);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "registrationDate", ignore = true)
     void updateCandidateFromDto(CandidateDto dto, @MappingTarget Candidate entity);
-
 }
--- src/main/java/com/syncteam/buscaEmpregoPAOO/mappers/VacancyMapper.java
+++ src/main/java/com/syncteam/buscaEmpregoPAOO/mappers/VacancyMapper.java
@@ -1,18 +1,20 @@
 package com.syncteam.buscaEmpregoPAOO.mappers;
 
-import com.syncteam.buscaEmpregoPAOO.dtos.VacancyDto;
-import com.syncteam.buscaEmpregoPAOO.entities.Vacancies;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
+import com.syncteam.buscaEmpregoPAOO.dtos.VacancyDto;
+import com.syncteam.buscaEmpregoPAOO.entities.Vacancies;
+
 @Mapper(componentModel = "spring")
 public interface VacancyMapper {
-
+    @Mapping(target = "creationDate", ignore = true)
     Vacancies toEntity(VacancyDto dto);
 
     VacancyDto toDTO(Vacancies vacancy);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationDate", ignore = true)
     void updateVacancyFromDto(VacancyDto dto, @MappingTarget Vacancies entity);
 }
